c32dbb75595ff616b6910d02ba94f4d543131bca
Magdalena Kierat <m.kierat@gmx.de>
2022-06-10 16:38:53 +0200

    update class naming

--- js/bookmark.js
+++ js/bookmark.js
@@ -2,7 +2,7 @@ function toggleBookmark() {
   const bookmarks = document.querySelectorAll("[data-js=bookmark]");
   bookmarks.forEach((bookmark) => {
     bookmark.addEventListener("click", () => {
-      bookmark.classList.toggle("card__bookmark--not-active");
+      bookmark.classList.toggle("card__bookmark--active");
     });
   });
 }
