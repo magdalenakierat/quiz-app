98cc28a6069dfd8359fa5d2abd4a9c836dcae629
Magdalena Kierat <m.kierat@gmx.de>
2022-06-12 15:59:48 +0200

    add text change on answer button

--- js/answer.js
+++ js/answer.js
@@ -1,19 +1,3 @@
-// function toggleAnswer(button_name, answer_name) {
-//   const buttonIdentifier = "[data-js=" + button_name + "]";
-//   const clickButton = document.querySelector(buttonIdentifier);
-//   const answerIdentifier = "[data-js=" + answer_name + "]";
-//   const answerElement = document.querySelector(answerIdentifier);
-//   clickButton.addEventListener("click", () =>
-//     answerElement.classList.toggle("hidden")
-//   );
-// }
-
-// toggleAnswer("button1", "answer1");
-// toggleAnswer("button2", "answer2");
-// toggleAnswer("button3", "answer3");
-
-// export { toggleAnswer };
-
 function toggleAnswer() {
   const cards = document.querySelectorAll("[data-js=card]");
   cards.forEach((card) => {
@@ -21,6 +5,11 @@ function toggleAnswer() {
     const answer = card.querySelector("[data-js=answer]");
     answerButton.addEventListener("click", () => {
       answer.classList.toggle("hidden");
+      if (answerButton.innerHTML == "Show answer") {
+        answerButton.innerHTML = "Hide answer";
+      } else {
+        answerButton.innerHTML = "Show answer";
+      }
     });
   });
 }
