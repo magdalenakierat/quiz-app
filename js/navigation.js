715acf95cd36c21128136e158ba8ec33cf11322b
Magdalena Kierat <m.kierat@gmx.de>
2022-06-13 12:50:34 +0200

    reduce code for navigation.js even more

--- js/navigation.js
+++ js/navigation.js
@@ -1,48 +1,19 @@
 function navigation() {
-  const linkHome = document.querySelector("[data-js=link-home]");
-  const linkBookmarks = document.querySelector("[data-js=link-bookmarks]");
-  const linkInput = document.querySelector("[data-js=link-input]");
-  const linkProfile = document.querySelector("[data-js=link-profile]");
+  const pages = document.querySelectorAll(".page");
+  const navLinks = document.querySelectorAll("[data-js=nav-link]");
+  const headline = document.querySelector("[data-js=h1-content]");
 
-  const pageHome = document.querySelector("[data-js=home]");
-  const pageBookmarks = document.querySelector("[data-js=bookmarks]");
-  const pageInput = document.querySelector("[data-js=input]");
-  const pageProfile = document.querySelector("[data-js=profile]");
-
-  const h1Content = document.querySelector("[data-js=h1-content]");
-
-  const allPages = document.querySelectorAll(".page");
-
-  linkHome.addEventListener("click", () => {
-    allPages.forEach((page) => {
-      page.classList.remove("current-page");
+  navLinks.forEach((navLink) => {
+    navLink.addEventListener("click", () => {
+      const selectedPage = document.querySelector(
+        `[data-js=${navLink.dataset.link}]`
+      );
+      pages.forEach((page) => {
+        page.classList.remove("current-page");
+      });
+      selectedPage.classList.add("current-page");
+      headline.textContent = navLink.dataset.headline;
     });
-    pageHome.classList.add("current-page");
-    h1Content.innerHTML = "QUIZ APP";
-  });
-
-  linkBookmarks.addEventListener("click", () => {
-    allPages.forEach((page) => {
-      page.classList.remove("current-page");
-    });
-    pageBookmarks.classList.add("current-page");
-    h1Content.innerHTML = "QUIZ APP – Bookmarks";
-  });
-
-  linkInput.addEventListener("click", () => {
-    allPages.forEach((page) => {
-      page.classList.remove("current-page");
-    });
-    pageInput.classList.add("current-page");
-    h1Content.innerHTML = "QUIZ APP – Create";
-  });
-
-  linkProfile.addEventListener("click", () => {
-    allPages.forEach((page) => {
-      page.classList.remove("current-page");
-    });
-    pageProfile.classList.add("current-page");
-    h1Content.innerHTML = "QUIZ APP – Profile";
   });
 }
 
